5ed8c015772243de72a0bf2ca4cf9b22caa78e87
majoyz <zmj-miss@live.com>
2018-04-09 17:21:11 +0800

    add 20180409

---
+++ 20180409/ceshi.cpp
@@ -0,0 +1,14 @@
+ ///
+ /// @file    ceshi.c
+ /// @author  lemon(haohb13@gmail.com)
+ /// @date    2018-04-09 16:53:15
+ ///
+ 
+#include <iostream>
+using std::cout;
+using std::endl;
+
+int main(){
+	cout<<"Can you see me?"<<endl;
+	return 0;
+}
