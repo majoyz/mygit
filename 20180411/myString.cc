500f511f2bced7de77fb5d96885bbe4e5dcdeb43
majoyz <zmj-miss@live.com>
2018-04-12 12:03:27 +0800

    add 20180411

---
+++ 20180411/myString.cc
@@ -0,0 +1,74 @@
+ ///
+ /// @file    myString.cc
+ /// @author  majoyz(zmj-miss@live.com)
+ /// @date    2018-04-11 20:16:20
+ ///
+ 
+#include <iostream>
+#include <string>
+#include <string.h>
+using std::cout;
+using std::endl;
+using std::string;
+
+class String{
+	public:
+		String()
+		:_pstr(new char[1]())
+		{
+			*_pstr=0;
+			cout<<"String()"<<endl;
+		}
+
+		String(const char *pstr)
+		:_pstr(new char[strlen(pstr)+1]())
+		{
+			::strcpy(_pstr,pstr);
+			cout<<"String(const char *)"<<endl;
+		}
+
+		String(const String & rhs)
+		:_pstr(new char[strlen(rhs._pstr)+1]())
+		{
+			::strcpy(_pstr,rhs._pstr);
+			cout<<"const String & rhs"<<endl;
+		}
+
+		String & operator=(const String & rhs)
+		{
+			_pstr=rhs._pstr;
+			cout<<"String & operator"<<endl;
+			return(*this);
+		}
+		
+		~String(){
+			delete [] _pstr;
+			cout<<"~String"<<endl;
+		}
+
+		void print(){
+			cout<<"I'm "<<_pstr<<endl;
+		}
+
+	private:
+		char * _pstr;
+};
+
+int main(){
+	String str1;
+	str1.print();
+
+	String str2 = "Hello,world";
+	String str3("wangdao");
+
+	str2.print();
+	str3.print();
+
+	String str4 = str3;
+	str4.print();
+
+	str4 = str2;
+	str4.print();
+
+	return 0;
+}
